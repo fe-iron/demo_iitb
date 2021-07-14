bafefe4bb6b5376b0ec054bac4b28fd503783a0a
fe-iron <elahifaiz00@gmail.com>
2021-07-14 15:16:48 +0545

    changes for deployment

--- webDevelopmentAssignment_backend_july2021/settings.py
+++ webDevelopmentAssignment_backend_july2021/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&x&)^bjwuhqz)$kl4_r=%r%hqyh@lr93@uit4fgk8cnw-yoznj'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["iitb-task.herokuapp.com", "127.0.0.1"]
 
