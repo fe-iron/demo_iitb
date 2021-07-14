4db0a4a6a7d0bd5f116b5613427eb4321faeb857
fe-iron <elahifaiz00@gmail.com>
2021-07-15 00:22:33 +0545

    addition in whitenoise

--- webDevelopmentAssignment_backend_july2021/settings.py
+++ webDevelopmentAssignment_backend_july2021/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # user agent middleware
     'django_user_agents.middleware.UserAgentMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webDevelopmentAssignment_backend_july2021.urls'
@@ -134,7 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
